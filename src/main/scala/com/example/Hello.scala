4cbe80d13b63416839fae4d7c9e0e81c4c313d77
shigemk2 <i.am.shige@gmail.com>
2016-03-19 22:41:52 +0900

    Add jedis-lock

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,19 @@
 package com.example
 
+import com.github.jedis.lock.JedisLock
+import redis.clients.jedis.Jedis
+
 object Hello {
   def main(args: Array[String]): Unit = {
+    val jedis = new Jedis("localhost")
+    val lock = new JedisLock(jedis, "lockname", 10000, 30000)
+    lock.acquire()
+    try {
+      // do some stuff
+    }
+    finally {
+      lock.release()
+    }
     println("Hello, world!")
   }
 }
