a7c36c689bc2bd5401dcc452887ef785f6112aeb
shigemk2 <i.am.shige@gmail.com>
2016-03-21 13:57:21 +0900

    Add Scalaz and rng

--- build.sbt
+++ build.sbt
@@ -7,6 +7,8 @@ scalaVersion := "2.11.7"
 // Change this to another test framework if you prefer
 libraryDependencies ++= Seq(
   "com.github.jedis-lock" % "jedis-lock" % "1.0.0",
+  "com.nicta" %% "rng" % "1.3.0",
+  "org.scalaz" %% "scalaz-core" % "7.2.1",
   "org.scalatest" %% "scalatest" % "2.2.4" % "test"
 )
 
--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -2,6 +2,9 @@ package com.example
 
 import com.github.jedis.lock.JedisLock
 import redis.clients.jedis.Jedis
+import com.nicta.rng.{Rng, Size}
+
+import scalaz.syntax.monad._
 
 object Hello {
   def main(args: Array[String]): Unit = {
@@ -9,8 +12,22 @@ object Hello {
     val lock = new JedisLock(jedis, "lockname", 10000, 30000)
     lock.acquire()
     try {
+      val rngInt: Rng[Int] = Rng.int
+      val rngStr: Rng[String] = Rng.string(Size(10))
+      val kv: Rng[(Int, String)] = for {
+        k <- rngInt
+        v <- rngStr
+      } yield (k, v)
       // do some stuff
-      jedis.set("key", "value")
+      kv.iterateUntil {
+        case (k, v) => k < 10
+      }.map {
+        case (k, v) => {
+          println(k)
+          println(v)
+          jedis.set(k.toString, v)
+        }
+      }
     }
     finally {
       lock.release()
