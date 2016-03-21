c275d778fb09a1e84d20b859c677854ab14733b1
shigemk2 <i.am.shige@gmail.com>
2016-03-21 17:07:14 +0900

    乱数はとりあえずこれで着地点

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -10,33 +10,18 @@ import scalaz.syntax.monad._
 
 object Hello {
   def main(args: Array[String]): Unit = {
-    // val jedis = new Jedis("192.168.99.100")
-    // val lock = new JedisLock(jedis, "lockname", 10000, 30000)
-    // lock.acquire()
-    // try {
-    //   val rngInt: Rng[Int] = Rng.int
-    //   val rngStr: Rng[String] = Rng.string(Size(10))
-    //   val kv: Rng[(Int, String)] = for {
-    //     k <- rngInt
-    //     v <- rngStr
-    //   } yield (k, v)
-    //   // do some stuff
-    //   kv.iterateUntil {
-    //     case (k, v) => k < 10
-    //   }.map {
-    //     case (k, v) => {
-    //       println(k)
-    //       println(v)
-    //       jedis.set(k.toString, v)
-    //     }
-    //   }
-    // }
-    // finally {
-    //   lock.release()
-    // }
-    val k = Cord(new scala.util.Random(new java.security.SecureRandom()).alphanumeric.take(10).mkString)
-    val v = new scala.util.Random(new java.security.SecureRandom()).nextInt(10000)
-    println(k, v)
+    val jedis = new Jedis("192.168.99.101")
+    val lock = new JedisLock(jedis, "lockname", 10000, 30000)
+    lock.acquire()
+    try {
+      val k = Cord(new scala.util.Random(new java.security.SecureRandom()).alphanumeric.take(10).mkString)
+      val v = new scala.util.Random(new java.security.SecureRandom()).nextInt(10000)
+      println(k, v)
+      jedis.set(k.toString, v.toString)
+    }
+    finally {
+      lock.release()
+    }
     println("Hello, world!")
   }
 }
