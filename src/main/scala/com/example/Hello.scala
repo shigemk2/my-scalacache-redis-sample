3a36baa2c57419ae1d4913b1b0ace548c0f4db63
shigemk2 <i.am.shige@gmail.com>
2016-04-24 15:53:51 +0900

    Fix

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -4,32 +4,18 @@ import org.joda.time.format.DateTimeFormat
 import redis.clients.jedis._
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 import scalacache._
 import scalacache.redis.RedisCache
-import scala.concurrent.ExecutionContext.Implicits.global
 
 object Hello {
   def main(args: Array[String]): Unit = {
     val jedis = new Jedis("192.168.99.100", 6379)
     implicit val scalaCache = ScalaCache(RedisCache("192.168.99.100", 6379))
-    // val startGet = new DateTime()
-    // println(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").print(startGet))
-    // for(i <- 1 to 100000) {
-    //   jedis.get(i.toString)
-    // }
-    // val endGet = new DateTime()
-    // println(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").print(endGet))
-    val result = Await.result({
-      scalacache.put("key1")("123", Option(Duration.Inf)).map { (Unit) =>
-        scalacache.get("key1".getBytes("utf-8"))
-      }
-    }, Duration.Inf)
-    println(result)
-    scalacache.put("1")(123, None)
-    println(scalacache.get("1"))
-
-    // val endCacheGet = new DateTime()
-    // println(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").print(endCacheGet))
+    jedis.set("key1", "123")
+    println(scalaCache.cache.get("key1"))
+    scalaCache.cache.put("key1", 123, Option(Duration.apply(60, MINUTES)))
+    println(scalaCache.cache.get("key1"))
     println("Hello, world!")
   }
 }
